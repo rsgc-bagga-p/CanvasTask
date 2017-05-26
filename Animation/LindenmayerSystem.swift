0264a7c8db8158b0a0a3b7cc0f16221f30286b0f
rsgc-bagga-p <pbagga@rsgc.on.ca>
2017-05-26 01:10:21 -0400

    Finished new function that randomizes the rule based on probability. Outputs a rule. This is a new and much easier way to do this than the previous way.

--- Animation/LindenmayerSystem.swift
+++ Animation/LindenmayerSystem.swift
@@ -4,21 +4,15 @@ public class LindenmayerSystem {
     // Set up required information
     var angle : Degrees                 // rotation amount for turtle (degrees)
     var axiom : String
-    var rule : [Character : [Successor]]
+    var rule : [Character : [String]]
     var n : Int                         // number of times the production rule is applied
     var word : [String] = []            // the word that will be rendered
                                         // is rendered with an animation, step by step
-    //let successor = Successor(rule: [""], odds: [])
-    var runningTotal : Float
-    var probability : Float
-    var currentRule : String
-    //var wordMultiple : [[String]] = []  // 2D array to hold all the multiple system words
-    //var ruleMultiple : [Character : String] // dictionary to hold the multiple rules
+
     public init(angle : Degrees,
                 axiom : String,
-                rule : [Character : [Successor]],
+                rule : [Character : [String]],
                 generations : Int
-                //ruleMultiple : [Character: String] = [:]
                 ) {
         
         // Initialize stored properties
@@ -26,12 +20,10 @@ public class LindenmayerSystem {
         self.axiom = axiom
         self.rule = rule
         self.n = generations
-        //self.ruleMultiple = ruleMultiple
         self.word.append(axiom)   // The first word is the axiom
-        //self.wordMultiple.append([axiom]
         
-//        // Apply the production rule
-        //applyRules()
+        // Apply the production rule
+        applyRules()
         
     }
     
@@ -45,13 +37,12 @@ public class LindenmayerSystem {
         self.rule = system.rule
         self.n = system.n
         self.word.append(axiom)   // The first word is the axiom
-        //self.ruleMultiple = system.ruleMultiple
         // Apply the production rule
-        //applyRules()
+        applyRules()
     }
     
     
-    func applyRules(with system: Successor) {
+    func applyRules() {
         
         // See if word needs to be re-written
         if n > 0 {
@@ -66,22 +57,9 @@ public class LindenmayerSystem {
                 for character in word[i - 1].characters {
                     
                     if rule[character] != nil {
-                    
-                        let random = Float(arc4random_uniform(1))
-                        print(random)
-                        
-                        for j in 1...system.rule.count {
-                            
-                            if random < system.probability[j] && random >= system.probability[j-1] {
-                                
-                                currentRule = system.rule[j-1]
-                                
-                            }
-                            
-                        }
                         
                         // apply production rule, replace "old" F with new string
-                        newWord.append(currentRule)
+                        newWord.append(rule[character]!)
                         
                     } else {
                         
@@ -97,12 +75,6 @@ public class LindenmayerSystem {
             }
             
         }
-    }
-    
-    func chooseRules() {
-        
-        
-        
     }
     
 }
--- Animation/Successor.swift
+++ Animation/Successor.swift
@@ -8,40 +8,95 @@
 
 import Foundation
 
-public struct Successor {
-    var rule : [String]
-    var odds : [Float]
-    var probability : [Float]
-    var runningTotal : Float
+//public class Successor {
+//    var rule : [String]
+//    var odds : [Float]
+//    var probability : [Float]
+//    var runningTotal : Float
+//    
+//    public init(rule: [String],
+//                odds: [Float],
+//                probability: [Float] = [],
+//                runningTotal: Float = 0.0) {
+//        
+//        self.rule = rule
+//        self.odds = odds
+//        self.probability = probability
+//        self.runningTotal = runningTotal
+//        chooseSuccessor()
+//    }
+//    
+//    
+//    func chooseSuccessor() -> String {
+//        var successor: String = ""
+//        
+//        //calculate probability
+//        for value in odds {
+//            runningTotal += value
+//        }
+//        
+//        for (index,_) in rule.enumerated() {
+//            
+//            probability.append((odds[index]/runningTotal))
+//            
+//        }
+//        
+//        let randomVal = Float(arc4random_uniform(100))
+//        let probabilityVal = randomVal/1000
+//        
+//        for j in 1...rule.count {
+//            
+//            if probabilityVal < probability[j] && probabilityVal >= probability[j-1] {
+//                
+//                successor = rule[j-1]
+//                
+//            }
+//            
+//        }
+//       
+//        return successor
+//        
+//    }
+//    
+//    }
+
+
+public func chooseSuccessor(input: [String]) -> String {
     
-    public init(rule: [String],
-                odds: [Float]) {
+    var newArray : [String] = []
+    var chosenRule : String = ""
+    var odds : [Int] = []
+    //var probability : [Float] = []
+    
+    //create the odds for each rule
+    for rule in input {
         
-        self.rule = rule
-        self.odds = odds
+        let probabilityRule = rule.components(separatedBy: "/")
         
-        calculateProbability()
+        odds.append(Int(probabilityRule[0])!)
+        
+        
     }
+
     
-    
-    func chooseSuccessor() {
+    //create the new array of rules, each with different amounts of the same rule
+    for i in 1...odds.count {
         
-        
-        
-        
-        
-    }
-    
-    mutating func calculateProbability() {
-        
-        for value in odds {
-            runningTotal += value
+        for j in 0...odds[i] {
+            
+            newArray.append(input[i])
+            
         }
         
-        for (index,_) in rule.enumerated() {
-            
-            probability[index] = (odds[index]/runningTotal)
-            
-        }
     }
+    
+    //choose a random number and return a string
+    let random = Int(arc4random_uniform(UInt32(newArray.count)))
+    
+    //set the new rule the random chosen
+    chosenRule = newArray[random]
+    
+    //return the new rule
+    return chosenRule
+
 }
