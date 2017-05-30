f86055199a08e1d532ae992bc4ab8339c46037a0
rsgc-bagga-p <pbagga@rsgc.on.ca>
2017-05-30 02:11:23 -0400

    I think the file analyzer is done. Going to test now.

--- Animation/FileReader.swift
+++ Animation/FileReader.swift
@@ -10,22 +10,22 @@ import Foundation
 
 class FileReader {
     
-    struct systemData {
-        
-        var xVal: Float = 0.0
-        var yVal: Float = 0.0
-        var axiom: String = ""
-        var rules: [Character : [String]] = [:]
-        var generations: Int = 0
-        var direction: Float = 0
-        var length: Float = 0
-        var lengthReduction: Float = 0
-        var thickness: Float = 0
-        var thicknessReduction: Float = 0
-        var angle: Degrees = 0
-        var colours: [String : Color] = [:]
-        
-    }
+//    struct systemData {
+//        
+//        var xVal: Float = 0.0
+//        var yVal: Float = 0.0
+//        var axiom: String = ""
+//        var rules: [Character : [String]] = [:]
+//        var generations: Int = 0
+//        var direction: Float = 0
+//        var length: Float = 0
+//        var lengthReduction: Float = 0
+//        var thickness: Float = 0
+//        var thicknessReduction: Float = 0
+//        var angle: Degrees = 0
+//        var colours: [Character : LineColor] = [:]
+//        
+//    }
     
     let path : String
     
@@ -35,9 +35,8 @@ class FileReader {
         
     }
     
-    var lsystems : [VisualizedLindenmayerSystem] = []
     
-    func readFile() -> [VisualizedLindenmayerSystem] {
+    func analyzeFile() -> [VisualizedLindenmayerSystem] {
         
         guard let reader = LineReader(path: path) else {
             
@@ -46,76 +45,125 @@ class FileReader {
             
         }
         
-        
+        var lsystems : [VisualizedLindenmayerSystem] = [] //an array for the systems created
         var components : [String] = [] //an array for the components of the file
         
         
         //interpret each line of the file
         for (_, line) in reader.enumerated() {
             
-            components.append(contentsOf: line.components(separatedBy: "\r"))
+            let lineData = line.components(separatedBy: " ")
+            let cleanedLineData = lineData[0].components(separatedBy: "\n")
             
+            if cleanedLineData[0] == "]"{
+                
+                lsystems.append(createLSystems(value: components))
+                components = []
+                
+            }
+            
+            components.append(line)
             
         }
         
-        return createLSystems(value: components)
+        return lsystems
     }
     
     
-    func createLSystems(value: [String]) -> [VisualizedLindenmayerSystem] {
+    func createLSystems(value: [String]) -> VisualizedLindenmayerSystem {
         
-        var system : systemData
+        var xVal: Float = 0.0
+        var yVal: Float = 0.0
+        var axiom: String = ""
+        var rules: [Character : [String]] = [:]
+        var generations: Int = 0
+        var direction: Int = 0
+        var length: Float = 0
+        var lengthReduction: Float = 1
+        var thickness: Float = 1
+        var thicknessReduction: Float = 0
+        var angle: Degrees = 0
+        var colours: [Character : LineColor] = [:]
         
         for (lineNumber, line) in value.enumerated() {
             
             //need to clean up the value so there are no issues
             var data = line.components(separatedBy: "\n")
             var dataType = data[0].components(separatedBy: ":")
-            
+            print(dataType[0])
             
             switch dataType[0] {
                 
             case "angle" :
-                system.angle = Degrees(Int(dataType[1])!)
+                angle = Degrees(Int(dataType[1])!)
                 break
             case "axiom" :
-                system.axiom = dataType[1]
+                axiom = dataType[1]
                 break
             case "rules" :
-                system.rules
+                var adder: Int = 2
+                while value[lineNumber + adder] != "}" {
+                    
+                    var newRule = value[lineNumber + 2].components(separatedBy: "\n")
+                    var currentRule = newRule[0].components(separatedBy: "=")
+                    let currentIndex = Character(currentRule[0])
+                    
+                    rules[currentIndex]?.append(currentRule[1])
+                    
+                    adder += 1
+                }
                 break
             case "generations" :
-                system.generations = Int(dataType[1])!
+                generations = Int(dataType[1])!
                 break
             case "colors" :
-                system.colours
+                var lineChange: Int = 2
+                while value[lineNumber + lineChange] != "}" {
+                    
+                    var newColourRule = value[lineNumber + 2].components(separatedBy: "\n")
+                    var currentColourRule = newColourRule[0].components(separatedBy: "=")
+                    let currentColourIndex = currentColourRule[0]
+                    var specificColours = currentColourRule[1].components(separatedBy: ",")
+                    
+                    let hue = Float(specificColours[0])!
+                    let saturation = Float(specificColours[1])!
+                    let brightness = Float(specificColours[2])!
+                    
+                    colours[Character(currentColourIndex)] = LineColor(hue: hue, saturation: saturation, brightness: brightness)
+                    
+                    lineChange += 1
+                }
                 break
             case "direction" :
-                system.direction = Float(dataType[1])!
+                direction = Int(dataType[1])!
                 break
             case "length" :
-                system.length = Float(dataType[1])!
+                length = Float(dataType[1])!
                 break
             case "length_reduction" :
-                system.lengthReduction = Float(dataType[1])!
+                lengthReduction = Float(dataType[1])!
                 break
             case "thickness" :
-                system.thickness = Float(dataType[1])!
+                thickness = Float(dataType[1])!
                 break
             case "thickness_reduction" :
-                system.thicknessReduction = Float(dataType[1])!
+                thicknessReduction = Float(dataType[1])!
                 break
             case "x" :
-                system.xVal = Float(x)!
+                xVal = Float(dataType[1])!
                 break
             case "y" :
-                system.yVal = Float(y)!
+                yVal = Float(dataType[1])!
+                break
+            default :
                 break
             }
             
         }
         
-        return lsystems
+        let system : LindenmayerSystem = LindenmayerSystem(angle: angle, axiom: axiom, rule: rules, generations: generations)
+        let visualizsedSystem : VisualizedLindenmayerSystem = VisualizedLindenmayerSystem(with: system, length: length, reduction: lengthReduction, x: xVal, y: yVal, direction: direction, colors: colours, thickness: thickness, thicknessReduction: thicknessReduction)
+        return visualizsedSystem
     }
     
 }
